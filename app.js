c7bd2906031fc51ff31c49db1f1aaac98b99de9c
happy-le <1113632076@qq.com>
2021-08-03 11:00:43 +0800

    中间件统一处理配置

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 /* 项目启动的相关初始化配置 */
-require('module-alias/register'); // 配置别名依赖
+require("module-alias/register"); // 配置别名依赖
 
-const Koa = require("koa")
-const router = require("@/router")
-const globalError = require("@/middleware/globalError")
+const Koa = require("koa");
+const router = require("@/router");
+const middleware = require("@/middleware/middleware");
 
 const app = new Koa();
-
-app.use(globalError()); // 将异常处理注册到全局
-
+middleware(app); // 加载全局中间件
 router(app); // 加载全局路由
 
 export default app;
